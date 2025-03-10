7d2962ed29faee1c1c96f4d5dc23f28d1670ea5d
korgentine <korgentine@gmail.com>
2025-03-09 23:15:53 -0400

    firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,14 @@
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgtiM3ER1pkDQEaM1jGqtnQxZqqW6gJ4o",
-  authDomain: "pretzeldaily-c340a.firebaseapp.com",
-  projectId: "pretzeldaily-c340a",
-  storageBucket: "pretzeldaily-c340a.firebasestorage.app",
-  messagingSenderId: "754918040979",
-  appId: "1:754918040979:web:a3fdf590f0cd349e433466",
-  measurementId: "G-QLLN5XE6R5"
+    apiKey: "AIzaSyBgtiM3ER1pkDQEaM1jGqtnQxZqqW6gJ4o",
+    authDomain: "pretzeldaily-c340a.firebaseapp.com",
+    projectId: "pretzeldaily-c340a",
+    storageBucket: "pretzeldaily-c340a.firebasestorage.app",
+    messagingSenderId: "754918040979",
+    appId: "1:754918040979:web:a3fdf590f0cd349e433466",
+    measurementId: "G-QLLN5XE6R5",
+    databaseURL: "https://pretzeldaily-c340a-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
